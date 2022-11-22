162336fe49952243772dde6d2e269c45138feebd
Irina <ira@berlic.net>
2022-11-22 14:14:47 +0300

    Lesson2 add

--- 22/Program.cs
+++ 22/Program.cs
@@ -1,27 +1,22 @@
 ﻿// 22. Программа проверяет пятизначное число на палиндромом.
 
-/*
-System.Console.WriteLine("Введите пятизначное число: ");
-System.Console.Read();
-
-int n=Convert.ToInt32(Console.ReadLine());
-int d1=n/10;
-int d2=n%10;
-int d3=n/100;
-int d4=n%100;
-
-if (d1==d2 && d3==d4)
-System.Console.WriteLine($"{n} является палидромом");
-else
-System.Console.WriteLine($"{n} НЕ является палидромом");
-*/
-/*
 System.Console.WriteLine("Введите пятизначное число: ");
 
-int n=Convert.ToInt32(Console.ReadLine());
-int temp=n;
-int converse=0;
-
-int d2=n%10;
-*/
+int originNumb=Convert.ToInt32(Console.ReadLine());
+int temp=Convert.ToInt32(Console.ReadLine());
+int newNumb=Convert.ToInt32(Console.ReadLine());
+int i=0;
+temp=originNumb;
+newNumb=0;
+i=0;
+while(i<=5)
+{
+    newNumb[i]=originNumb%10*10000+originNumb/10;
+    i=i+1;
+}
+System.Console.WriteLine($"{newNumb}");
+{
+if(newNumb==temp) System.Console.WriteLine($"{originNumb} является палидромом");
+else System.Console.WriteLine($"{originNumb} НЕ является палидромом");
+}
 
