444fadf408bb0dc87e7a6a380c6838aef0989c70
Irina <ira@berlic.net>
2022-12-02 17:38:32 +0300

    Lesson5

---
+++ 51/Program.cs
@@ -0,0 +1,16 @@
+﻿// 51.	С клавиатуры вводится число N. Затем вводятся N чисел. 
+// Определить сколько чисел больше 0 введено с клавиатуры
+
+int result = 0;
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+    for (int i = 1; i <= n; i++)
+    {
+    Console.WriteLine($"Введите число {i} из {n}: ");
+    int input = Convert.ToInt32(Console.ReadLine());
+    
+        if (input > 0)
+        result++;
+    }
+Console.WriteLine($"{result} чисел из {n} больше нуля");
