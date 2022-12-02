406b05eb1d8dde75c91af870f73f36af301c7167
Irina <ira@berlic.net>
2022-12-02 16:45:21 +0300

    Lesson 5

---
+++ 54/Program.cs
@@ -0,0 +1,31 @@
+﻿// 54. С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. 
+//Принять первые числа равными 0 и 1
+
+int[] Fibonacci(int n)
+{
+    int[] result = new int[n];
+    for (int i = 0; i < n; i++)
+        if (i < 2) result[i] = i;
+        else result[i] = result[i - 1] + result[i - 2];
+    return result;
+}
+
+{
+    Console.WriteLine("Ввести число N: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int[] numbers = Fibonacci(n);
+    for (int i = 0; i < numbers.Length; i++)
+        Console.Write($"{numbers[i]} ");
+}
+
+// Вариант преподавателя
+/*
+int n=10;
+int[] a=new int[n];
+a[0]=0;
+a[1]=1;
+for(int i=2;i<n;i++)
+  a[i]=a[i-1]+a[i-2];//рекурентная формула (следующее значение за счет использования предыдущих)
+for(int i=0;i<n;i++)
+    System.Console.Write($"{a[i]} ");
+*/
---
+++ 56/Program.cs
@@ -0,0 +1,33 @@
+﻿// 56.	Написать программу копирования массива
+
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write(arr[i] + " ");
+    Console.WriteLine();
+}
+
+int[] Copy(int[] arr)
+{
+    int length = arr.Length;
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+        result[i] = arr[i];
+    return result;
+}
+
+{
+    Random random = new Random();
+    Console.WriteLine("Введите число N: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    int[] array1 = new int[n];
+    for (int i = 0; i < n; i++)
+        array1[i] = random.Next(0, 100);
+    Console.Write("Массив случайных чисел: ");
+    Print(array1);
+
+    int[] array2 = Copy(array1);
+    Console.Write("Копия массива: ");
+    Print(array2);
+}
